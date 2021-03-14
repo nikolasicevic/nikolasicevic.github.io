32ccf8958f95faef4bc4c4f64cd658532627d693
Nikola Sicevic <nikolasicevic@live.com>
2021-03-14 12:44:02 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio | Nikola Sicevic</title>
+</head>
+<body>
+    <script type="text/javascript"></script>
+    <header>
+        <img>
+        <nav></nav>
+    </header>
+    <main>
+        <section class="home">
+            <h1>Nikola Sićević</h1>
+            <div>
+                <h3>About me*</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <ul>
+                <li>[ico] [phone]</li>
+                <li>[ico] [mail]</li>
+                <li>[ico] [skype]</li>
+                <li>[ico] [ln]</li>
+                <li>[ico] [github]</li>
+            </ul>
+        </section>
+        <section class="work">
+            <h2>work experience</h2>
+            <ul>
+                <li>
+                    <span>[position]</span>
+                    <span>[company]</span>
+                    <span>[start - end]</span>
+                    <h4>responsibilities</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    [project name]
+                    [project description]
+                    [stack]
+                </li>
+            </ul>
+        </section>
+        <section class="skills"></section>
+        <section class="edu">
+            <ul>
+                <li>
+                    [school][module][start - end]
+                </li>
+            </ul>
+        </section>
+        <section class="cert">
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </section>
+        <section class="intersests"></section>
+    </main>
+</body>
+</html>
